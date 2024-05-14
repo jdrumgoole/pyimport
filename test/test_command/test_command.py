91c3736d2a65cdb06db2e4aa2b25e3a2e0c15d0e
Joe Drumgoole <joe@joedrumgoole.com>
2024-05-14 15:27:26 +0100

    refactoring

---
+++ test/test_command/test_command.py
@@ -0,0 +1,102 @@
+import os
+import shutil
+import unittest
+from logging import getLogger
+
+import pymongo
+
+from pyimport.audit import Audit
+from pyimport.command import Drop_Command, GenerateFieldfileCommand, ImportCommand
+from pyimport.filesplitter import LineCounter
+
+path_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+def f(path):
+    return os.path.join(path_dir, path)
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self._client = pymongo.MongoClient()
+        self._database = self._client["TEST_CMD"]
+        self._collection = self._database["test"]
+        self._collection.insert_one({"hello": "world"})
+
+    def tearDown(self):
+        self._client.drop_database( "TEST_CMD")
+
+    def test_Drop_Command(self):
+        self._audit = Audit(database=self._client["TEST_AUDIT"])
+        batch_id = self._audit.start_batch({"test": "test_batch"})
+
+        cmd = Drop_Command(database=self._database,
+                           audit=self._audit,
+                           id=batch_id)
+
+        self.assertTrue(self._collection.find_one({"hello": "world"}))
+
+        cmd.run("test")
+
+        self.assertFalse(self._collection.find_one({"hello": "world"}))
+
+        self._audit.end_batch(batch_id)
+
+    def test_generate_field_file_Command(self):
+        cmd = GenerateFieldfileCommand()
+        shutil.copy("yellow_trip_data_10.csv",
+                    "test_generate.csv")
+        cmd.run("test_generate.csv")
+        self.assertTrue(os.path.isfile("test_generate.tff"))
+        os.unlink("test_generate.tff")
+        os.unlink("test_generate.csv")
+
+    def test_Import_Command(self):
+        self._audit = Audit(database=self._client["TEST_AUDIT"])
+        batch_id = self._audit.start_batch({"test": "test_batch"})
+        collection = self._database["import_test"]
+
+        start_size = collection.count_documents({})
+        size_10k = LineCounter("10k.txt").line_count
+        size_120 = LineCounter("120lines.txt").line_count
+        cmd = ImportCommand(audit=self._audit,
+                            id=batch_id,
+                            collection=collection,
+                            field_filename="10k.tff",
+                            delimiter="|",
+                            has_header=False,
+                            onerror="warn",
+                            limit=0)
+
+        cmd.run("10k.txt", "120lines.txt")
+        new_size = collection.count_documents({})
+        self.assertEqual(size_10k + size_120, new_size - start_size)
+
+        self._audit.end_batch(batch_id)
+
+    def test_Import_Command_new(self):
+        self._audit = Audit(database=self._client["TEST_AUDIT"])
+        batch_id = self._audit.start_batch({"test": "test_batch"})
+        collection = self._database["import_test"]
+
+        start_size = collection.count_documents({})
+        size_test = LineCounter("test_date_data.csv").line_count - 1
+        cmd = ImportCommand(audit=self._audit,
+                            id=batch_id,
+                            collection=collection,
+                            field_filename="10k.tff",
+                            delimiter=",",
+                            has_header=True,
+                            onerror="warn",
+                            limit=0)
+
+        cmd.run("test_date_data.csv")
+        new_size = collection.count_documents({})
+        self.assertEqual(size_test, new_size - start_size)
+
+        self._audit.end_batch(batch_id)
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.test_FieldConfig']
+    unittest.main()
